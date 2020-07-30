3effff716d8085f93042ea2951ab8eb643705fd1
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-30 03:57:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,64 @@
+require 'pry'
+
 class TicTacToe
   
-  def initialize
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  
+  WIN_COMBINATIONS = [
+    
+    [0, 1, 2],
+    [3, 4, 5], 
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    
+  ]
     
     
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  
+  def input_to_index(space)
+    space.to_i - 1 
+  end
+  
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  
+  def position_taken?(index)
+    
+    if @board[index] != " "
+      true 
+    else
+      false
+    end
     
   end
+    
+    
+  def valid_move?(position)
+    
+    index = input_to_index(position)
+    
+    index.between?(0,8) && !position_taken?(index)
+    
+  end
+      
+  #binding.pry 
   
 end
